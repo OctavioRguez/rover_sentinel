19fad97b0be8b58a61dad03164c5c91621b47c1f
OctavioRguez <octaviomacias16@hotmail.com>
2024-04-18 19:30:34 -0600

    fix: :bug: Changed folders name to avoid errors

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages = ['Classes'],
+    packages = ['Classes_Rover'],
     package_dir = {'': 'scripts'}
 )
 
